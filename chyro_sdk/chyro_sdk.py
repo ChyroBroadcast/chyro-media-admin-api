5b6f65f2be31ffdf8e7d2e8829c4c81571c9f150
Enzo Calamia <dev@sagebl.eu>
2017-04-13 12:06:16 +0200

    Add triggerbypost + send_email

--- chyro_sdk/chyro_sdk.py
+++ chyro_sdk/chyro_sdk.py
@@ -44,7 +44,7 @@ class Chyro(object):
         self.token = token
         self.host = host
         self.cj = cj
-	self.bc = bc
+        self.bc = bc
 
     def _make_url(self, module, resource, form='json'):
         host = self.host
@@ -61,7 +61,7 @@ class Chyro(object):
         return json.loads(res.read().decode('utf-8'))['data']
 
     def get(self, resource, **filters):
-	bc = str(self.bc)
+        bc = str(self.bc)
         url = self._make_url('get', resource)
         url += '?' + urlencode(filters)
         url += '&broadcast=' + bc
@@ -82,7 +82,7 @@ class Chyro(object):
         return res.read()
 
     def playlist(self, iso_date, norm):
-	bc = self.bc
+        bc = self.bc
         url = self._make_url('export', 'daily', norm)
         url += '?begin={iso_date}&bc={bc}'.format(**locals())
         res = self.opener.open(url)
@@ -93,7 +93,42 @@ class Chyro(object):
         url += '?id=' + media_id
         res = self.opener.open(url)
         return res.read()
-        
+
+    def triggerbypost(self, pgm_id, media_name, file_location):
+        print('triggerbypost')
+        post_params = urlencode({
+            'program_program_id': pgm_id,
+            'media_part': 1,
+            'media_endpart': 1,
+            'media_filelocation': file_location,
+            'mediaagentrepository_id': 16,
+            'media_filename': media_name, # Name in  IMPORT
+            'media_name': media_name,     # Name in Chyro DB
+            'media_version': 32,
+            'mediatype_mediatype_id': 9,
+            'physicalmedia_physicalmedia_id': 'all',
+            'workflowType': 'importMedia',
+            'targetType': 'program',
+            'targetQuery': ('{id==%s}' % pgm_id),
+        })
+        url = self._make_url('workflow', 'triggerbypost')
+        res = self.opener.open(url, data=post_params) # This is a POST
+        print(res)
+        print(post_params)
+        return True
+
+    def send_email(self, template, *params):
+        email_params = ','.join(str(p) for p in params)
+        url = self._make_url('mail', 'send')
+        qs = urlencode({
+            'template': template,
+            'params': email_params,
+        })
+        url += '?' + qs
+        print(url)
+        res = self.opener.open(url).read()
+        return res
+
 def print_json(data):
     ''' pretty-printer '''
     print(json.dumps(data,
